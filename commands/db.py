cfa37835db8a3259165ee89dc61b63edef43dfeb
Matt <workmatthewjharris94@gmail.com>
2022-02-08 16:59:27 +0000

    .

--- commands/db.py
+++ commands/db.py
@@ -50,7 +50,7 @@ def add_user(discord, gbp=0, exp=0, _inventory=[None]):
 	print("Adding", discord)
 	inventory = """{"inventory":!}""".replace("!", str(_inventory))
 	sql = """INSERT INTO accounts(DISCORD, GBP, EXP, INVENTORY)
-VALUES ({discord}, {gbp}, {exp} {inventory});"""
+VALUES ({discord}, {gbp}, {exp} '{inventory}');"""
 	sql = sql.format(discord=discord, gbp=gbp, exp=exp, inventory = inventory)
 	
 	conn = None
