e4be78e0c82744ba0f3993d78bf4d988944d6b8b
Matt <workmatthewjharris94@gmail.com>
2022-02-12 01:57:35 +0000

    .

--- commands/items/medals.py
+++ commands/items/medals.py
@@ -2,9 +2,6 @@ from .shop import SHOP, Item, define_item
 import datetime
 
 class Award(Item):
-	def __init__(self, name, recipient, reason):
-		super().__init__({"name":name, "value":1000, "cls":"Award", "reason": reason, "awarded":str(datetime.datetime.now().timestamp()), "recipient":recipient})
-		
 	def inspect(self, ctx, bot, user):
 		name = bot.get_user(user.discord).display_name
 		awarded = str(datetime.datetime.fromtimestamp(self.data["awarded"]))
