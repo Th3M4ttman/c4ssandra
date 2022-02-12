cb5368c5a43597b4c936a0b0e8808e4c0cbef498
Matt <workmatthewjharris94@gmail.com>
2022-02-12 01:55:18 +0000

    .

--- commands/db.py
+++ commands/db.py
@@ -472,12 +472,12 @@ async def medal(ctx):
 	cont = " ".join(parts)
 	recipient = cassandra.get_user(ctx.message.raw_mentions[0])
 	name, reason = cont.split("/")
-	item = construct({"name":name, "value":1000, "cls":"Award", "reason": reason, "awarded":str(datetime.datetime.now.timestamp()), "recipient":recipient.display_name})
+	item = construct({"name":name, "value":1000, "cls":"Award", "reason": reason, "awarded":datetime.datetime.now().timestamp(), "recipient":recipient.display_name})
 	u = CUser(recipient.id)
 	u.add_item(item)
 	msg = f"{recipient.name} was awarded a {item.name}\nitem.use(ctx=ctx, bot=cassandra, user=u)"
 	await ctx.message.channel.send(msg)
-	
+	await ctx.message.delete()
 	
 	
 	
--- commands/items/medals.py
+++ commands/items/medals.py
@@ -3,11 +3,12 @@ import datetime
 
 class Award(Item):
 	def __init__(self, name, recipient, reason):
-		super().__init__({"name":name, "value":1000, "cls":"Award", "reason": reason, "awarded":str(datetime.datetime.now.timestamp()), "recipient":recipient})
+		super().__init__({"name":name, "value":1000, "cls":"Award", "reason": reason, "awarded":str(datetime.datetime.now().timestamp()), "recipient":recipient})
 		
 	def inspect(self, ctx, bot, user):
 		name = bot.get_user(user.discord).display_name
-		return f"{name} shows off their {self.name}:\n\tReason: {self.data['reason']}\n\tAwarded on: {self.data['awarded'].split('.')[0]}"
+		awarded = str(datetime.datetime.fromtimestamp(self.data["awarded"]))
+		return f"{name} shows off their {self.name}:\n\tReason: {self.data['reason']}\n\tAwarded on: {awarded.split('.')[0]}"
 		
 	async def use(self, ctx, bot,  user, *args, **kwargs):
 		return False, self.inspect(ctx, bot, user)
