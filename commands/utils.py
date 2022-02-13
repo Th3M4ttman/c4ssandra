e250e0cce1207d8ea6473709eb78bbfa6a169e9e
Matt <workmatthewjharris94@gmail.com>
2022-02-13 23:30:29 +0000

    .

--- commands/utils.py
+++ commands/utils.py
@@ -5,11 +5,11 @@ async def choice(prompt, choices, ctx:Context, bot:Bot, timeout=0):
 	nums = ["0️⃣", "1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣"]
 	prompt = prompt + ":"
 	for i, choice in enumerate(choices):
-		prompt += "\n\t{nums[i]} - {choice}"
+		prompt += f"\n\t{nums[i]} - {choice}"
 	
 	msg = await ctx.channel.send(prompt)
 	for i, choice in enumerate(choices):
-		msg.add_reaction(nums[i])
+		await msg.add_reaction(nums[i])
 		
 	def check(reaction, user):
 		return user.id == ctx.author.id and reaction.emoji in nums
